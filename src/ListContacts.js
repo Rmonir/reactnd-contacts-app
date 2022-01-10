89d99283e59fdf1f5c842585b47793da59093583
Refat Monir <refat.mounir@gmail.com>
2022-01-11 02:02:00 +0300

    replace React component with function component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,34 +1,61 @@
-import React ,{Component} from  'react'
+import React  from  'react'
 
-class ListContacts extends Component {
-    render() {
-        console.log('Prop', this.props)
-        return (
-            <ol className='contact-list'>
-                {this.props.contacts.map((item) =>
-                (
-                    <li className='contact-list-item' key={item.id}>
-                        <div className='contact-avatar'
-                            style={{
-                                backgroundImage: `url(${item.avatarURL})`
-                            }}
-                        >
-                        </div>
-                        <div className='contact-details'>
-                            <p>
-                                {item.name}
-                            </p>
-                            <p>
-                                @{item.handle}
-                            </p>
-                        </div>
-                    </li>
-                )
-                )}
-            </ol>
-        )
-    }
+// class ListContacts extends Component {
+//     render() {
+//         console.log('Prop', this.props)
+//         return (
+//             <ol className='contact-list'>
+//                 {this.props.contacts.map((item) =>
+//                 (
+//                     <li className='contact-list-item' key={item.id}>
+//                         <div className='contact-avatar'
+//                             style={{
+//                                 backgroundImage: `url(${item.avatarURL})`
+//                             }}
+//                         >
+//                         </div>
+//                         <div className='contact-details'>
+//                             <p>
+//                                 {item.name}
+//                             </p>
+//                             <p>
+//                                 @{item.handle}
+//                             </p>
+//                         </div>
+//                     </li>
+//                 )
+//                 )}
+//             </ol>
+//         )
+//     }
+// }
+
+
+function ListContacts(props) {
+    return (
+        <ol className='contact-list'>
+            {props.contacts.map((item) =>
+            (
+                <li className='contact-list-item' key={item.id}>
+                    <div className='contact-avatar'
+                        style={{
+                            backgroundImage: `url(${item.avatarURL})`
+                        }}
+                    >
+                    </div>
+                    <div className='contact-details'>
+                        <p>
+                            {item.name}
+                        </p>
+                        <p>
+                            @{item.handle}
+                        </p>
+                    </div>
+                </li>
+            )
+            )}
+        </ol>
+    )
 }
-
 
 export default ListContacts
